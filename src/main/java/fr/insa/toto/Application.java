77dccca0d1e60e48c067d5832d9d120672ebefdb
beuvron <jeLeDonnePas@rien.com>
2026-09-21 23:54:45 +0200

    apresTomcat

--- src/main/java/fr/insa/toto/Application.java
+++ src/main/java/fr/insa/toto/Application.java
@@ -3,6 +3,8 @@ package fr.insa.toto;
 import com.vaadin.flow.theme.aura.Aura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
@@ -12,7 +14,14 @@ import com.vaadin.flow.component.page.Push;
 @StyleSheet(Aura.STYLESHEET)
 @StyleSheet("styles.css") // Your custom styles
 @Push
-public class Application implements AppShellConfigurator {
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
